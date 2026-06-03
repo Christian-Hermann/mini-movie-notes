88c07eed1b3543b5852ec24c2f92e3be4837b273
Your NameChristian <christianhermanndev@gmail.com>
2026-06-03 14:10:52 -0500

    Added movies array and first route

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,30 @@ const app = express();
 
 const PORT = 3000;
 
+const movies = [
+  {
+    id: 1,
+    title: "The Goonies",
+    note: "Best movie ever made.",
+  },
+  {
+    id: 2,
+    title: "Mad Max: Fury Road",
+    note: "Best action movie ever.",
+  },
+  {
+    id: 3,
+    title: "Alien Romulous",
+    note: "Can watch over and over again.",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Mini Movie Notes API");
+});
+
+app.get("/movies", (req, res) => {
+  res.json(movies);
 });
 
 app.listen(PORT, () => {
