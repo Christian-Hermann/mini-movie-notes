c008f25edeef51c326a3a1d3b341b16762003b07
Your NameChristian <christianhermanndev@gmail.com>
2026-06-06 12:46:39 -0500

    useEffect and map frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const response = await fetch("http://localhost:3000/movies");
+        const data = await response.json();
+        console.log(data);
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getMovies();
+  }, []);
 
   return (
     <main>
       <h1>Mini Movie Notes</h1>
-
-      <p>Count: {count}</p>
-
-      <button onClick={() => setCount(count + 1)}>Add One</button>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <strong>{movie.title}</strong>: {movie.note}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
-
 export default App;
