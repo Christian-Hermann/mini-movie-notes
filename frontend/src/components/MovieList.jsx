95d5adfd2465e8c7922b9cbaa1a9cd3de158965f
Your NameChristian <christianhermanndev@gmail.com>
2026-06-13 12:50:31 -0500

    Finished CSS

--- frontend/src/components/MovieList.jsx
+++ frontend/src/components/MovieList.jsx
@@ -2,7 +2,7 @@ function MovieList({ movies, onDelete, onEdit }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li key={movie.id}>
+        <li key={movie.id} className="movie-card">
           <strong>{movie.title}</strong>: {movie.note}
           <button onClick={() => onEdit(movie)}>Edit</button>
           <button onClick={() => onDelete(movie.id)}>Delete</button>
