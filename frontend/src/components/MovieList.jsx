aa819679db079a9be32a5e31a713eb8922b28845
Your NameChristian <christianhermanndev@gmail.com>
2026-06-16 16:54:17 -0500

    Small edit to notes

--- frontend/src/components/MovieList.jsx
+++ frontend/src/components/MovieList.jsx
@@ -3,7 +3,7 @@ function MovieList({ movies, onDelete, onEdit }) {
     <ul>
       {movies.map((movie) => (
         <li key={movie.id} className="movie-card">
-          <strong>{movie.title}</strong>: {movie.note}
+          <strong>{movie.title}</strong> {movie.note}
           <button onClick={() => onEdit(movie)}>Edit</button>
           <button onClick={() => onDelete(movie.id)}>Delete</button>
         </li>
